a9b662cd4a215d834de8d4fc3e3fa0b9fedc75d3
Happy Joseph Van Der Merwe <st10482408@imconnect.edu.za>
2026-08-18 20:54:33 +0200

    Models and interface commit
    I have created and worked on Models and interfaces, with MenuItems and Staff Documents completed. Inside the Functions folder I completed the "CreateMenuItemsFunction" function.

--- Interface/ItableStorageService.cs
+++ Interface/ItableStorageService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeAndChill.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,34 @@ namespace CoffeeAndChill.Interface
 {
     internal class ItableStorageService
     {
+        namespace Coffe_Chill.Functions.Interfaces
+    {
+        public interface ITableStorageService
+        {
+            Task<MenuItems> CreateMenuItemAsync(CreateMenuItemRequest request);
+
+            Task<List<MenuItems>> GetAllMenuItemsAsync();
+
+            Task<List<MenuItems>> GetMenuItemsByCatergoryAsync(string catergory);
+
+            Task<MenuItems> GetMenuItemsAsync(string catergory, string sku);
+
+            Task<MenuItems> UpdateMenuItemAsync(
+                string catergory,
+                string sku,
+                UpdateMenuItemRequest request
+
+                );
+            Task<bool> DeleteMenuItemAsync(
+                string catergory,
+                string sku
+                );
+
+
+
+
+        }
     }
+
+}
 }
