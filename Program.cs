4d90e08dff35c41d19fa6cfb4245c6dbd9addd8a
Kamva-Setheni <166064136+Kamva-Setheni@users.noreply.github.com>
2026-08-18 22:21:56 +0200

    2nd commit
     Initial commit of the part Kamva has done which includes all staff documents

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
+using CoffeeAndChill.Interface;   // ADD THIS
+using CoffeeAndChill.Services;    // ADD THIS
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+// ADD THIS BLOCK
+builder.Services.AddSingleton<IDocumentStorageService>(sp =>
+    new DocumentStorageService(
+        Environment.GetEnvironmentVariable("AzureWebJobsStorage") ?? "UseDevelopmentStorage=true"));
 
 if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
 {
